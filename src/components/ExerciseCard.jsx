731c8dd6dd9f2ce52496d4d45e8fca04994588ef
Vs-kishu <91000916+Vs-kishu@users.noreply.github.com>
2022-12-05 20:35:40 +0530

    first

---
+++ src/components/ExerciseCard.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Stack, Typography } from "@mui/material";
+
+const ExerciseCard = ({ exercise }) => {
+  return (
+    <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
+      <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+
+      <Stack direction="row">
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#fff",
+            background: "#ff8c00",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.bodyPart}
+        </Button>
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#fff",
+            background: "#000000",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.target}
+        </Button>
+      </Stack>
+
+      <Typography
+        ml="21px"
+        color="#000"
+        fontWeight="bold"
+        sx={{ fontSize: { lg: "24px", xs: "20px" } }}
+        mt="11px"
+        pb="10px"
+        textTransform="capitalize"
+        fontSize="22px"
+      >
+        {exercise.name}
+      </Typography>
+    </Link>
+  );
+};
+
+export default ExerciseCard;
---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Box, Typography, Stack, Button } from "@mui/material";
+import HeroBannerImage from "../assets/images/banner.png";
+
+const HeroBanner = () => {
+  return (
+    <Box
+      sx={{ mt: { lg: "212px", xs: "70px" }, ml: { sm: "50px" } }}
+      position="relative"
+      p="20px"
+    >
+      <Typography color="#000000" fontWeight="600" fontSize="26px">
+        Fitness Club
+      </Typography>
+      <Typography
+        color="#ff2625"
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+        mb="23px"
+        mt="30px"
+      >
+        Sweat, Smile <br />
+        And Repeat
+      </Typography>
+      <Typography
+        color="#808080"
+        fontSize="22px"
+        fontFamily="Alegreya"
+        lineHeight="35px"
+      >
+        Check out the most effective exercises personalized to you
+      </Typography>
+
+      <Button
+        variant="contained"
+        color="error"
+        href="#exercises"
+        sx={{ backgroundColor: "#000000", padding: "10px" }}
+      >
+        Explore Exercises
+      </Button>
+
+      <Typography
+        fontWeight={600}
+        color="#FF2625"
+        sx={{
+          opacity: "0.1",
+          display: { lg: "block", xs: "none" },
+          fontSize: "200px",
+        }}
+      >
+        Exercise
+      </Typography>
+      <img
+        src={HeroBannerImage}
+        alt="hero-banner"
+        className="hero-banner-img"
+      />
+    </Box>
+  );
+};
+
+export default HeroBanner;
